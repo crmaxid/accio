e24dce096915ee8554f3d2617656c49b646791c7
Rifandy Elfransyah Persulessy <rilfransyah@gmail.com>
2026-03-23 01:35:58 +0700

    feat: CIMS-4 - adding stock transaction list page

--- src/services/core/sku.ts
+++ src/services/core/sku.ts
@@ -1,4 +1,5 @@
 import { core } from '@/lib'
+import { SkuList, StockTransactionList } from '@/types'
 import { useQuery } from '@tanstack/react-query'
 
 const SKU_QUERY_KEY = 'sku'
@@ -8,17 +9,44 @@ interface SkuParams {
   limit: number
   code?: string
   productName?: string
+  startDate?: string
+  endDate?: string
 }
 
-export const useSku = ({ page, limit, code, productName }: SkuParams) => {
+export const useSku = ({
+  page,
+  limit,
+  code,
+  productName,
+  startDate,
+  endDate,
+}: SkuParams) => {
   const getSkuList = useQuery({
     queryKey: [SKU_QUERY_KEY, page, limit, code, productName],
     queryFn: async () =>
       await core
-        .get('/v1/sku', { params: { page, limit, code, productName } })
+        .get<SkuList>('/v1/sku', { params: { page, limit, code, productName } })
         .then((res) => res.data),
   })
+
+  const getStockTransaction = useQuery({
+    queryKey: [
+      `${SKU_QUERY_KEY}-stock-transaction`,
+      page,
+      limit,
+      startDate,
+      endDate,
+    ],
+    queryFn: async () =>
+      await core
+        .get<StockTransactionList>('/v1/sku/stock-transactions', {
+          params: { page, limit, startDate, endDate },
+        })
+        .then((res) => res.data),
+  })
+
   return {
     getSkuList,
+    getStockTransaction,
   }
 }
