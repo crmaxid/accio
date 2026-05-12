326b582810b2745e41410ecd4d64dd80132489d6
Rifandy Elfransyah Persulessy <rilfransyah@gmail.com>
2026-05-12 15:58:01 +0700

    fix: CIMS-27 - fixing the configuration

--- src/lib/config/config.ts
+++ src/lib/config/config.ts
@@ -1,3 +1,5 @@
 export const ENV = {
   MAPBOX_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_TOKEN,
+  GOOGLE_MAPS_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY,
+  API_BASE_URL: process.env.NEXT_PUBLIC_API_URL || '',
 } as const
